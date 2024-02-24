ab233b0fe57d62e7e735f8b6821cca3df5fb694a
JaumVituu <78977800+JaumVituu@users.noreply.github.com>
2024-02-24 09:02:01 -0300

    first commit

---
+++ blogPessoal/blogPessoal/src/main/java/fatec/ipiranga/blogPessoal/BlogPessoalApplication.java
@@ -0,0 +1,13 @@
+package fatec.ipiranga.blogPessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoalApplication.class, args);
+	}
+
+}
---
+++ blogPessoal/blogPessoal/src/test/java/fatec/ipiranga/blogPessoal/BlogPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package fatec.ipiranga.blogPessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
